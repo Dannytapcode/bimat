0c1978f023603917052738545d71e4c7a3f3aae0
Dannytapcode <151208488+Dannytapcode@users.noreply.github.com>
2024-08-08 10:25:28 +0700

    Add files via upload

--- love.js
+++ love.js
@@ -23,3 +23,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
     type();
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const text = `
+    Sau một khoảng thời gian nhắn tin và chia sẻ cùng nhau, mình cảm thấy thật may mắn khi có cậu trong cuộc sống. Đã có lúc mình nghĩ rằng nên dừng lại, nhưng chính sự hiện diện và tình cảm của cậu đã khiến mình nhận ra rằng không thể thiếu cậu. Mình đã và đang thay đổi bản thân mỗi ngày vì cậu. Tình cảm của mình dành cho cậu không chỉ là những lời nói, mà còn là những hành động, những nỗ lực để trở thành một người tốt hơn. Mình muốn được cùng cậu chia sẻ những niềm vui, nỗi buồn và cả những ước mơ trong tương lai. Nguyễn Hồng, mình thật sự yêu cậu và mong muốn chúng ta có thể tiến xa hơn trên con đường này. Cậu có đồng ý cùng mình bước tiếp không?
+    `;
+    const typingText = document.getElementById("typing-text");
+    let index = 0;
+  
+    function type() {
+      if (index < text.length) {
+        typingText.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(type, 50); // Điều chỉnh tốc độ gõ ở đây (đơn vị: milliseconds)
+      }
+    }
+  
+    type();
+  });
+  
